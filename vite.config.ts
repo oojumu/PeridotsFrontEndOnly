581b93167828d09d3b3e4f255e1651150835d6f1
oojum <oojumu@yahoo.com>
2024-11-20 00:53:39 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: '../API/wwwroot',
-    emptyOutDir: true
-  },
-
-  server: {
-    port: 3000
-  },  
   plugins: [react()],
 })
